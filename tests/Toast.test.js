b8daa486d6ef7de9682e012cc6dddd86ec9cb93e
Ben Rush <orionrush@gmail.com>
2022-10-31 18:22:55 +0000

    initial coverage

--- tests/Toast.test.js
+++ tests/Toast.test.js
@@ -1,31 +1,43 @@
-import { Satchel } from '../src/Satchel'
+import { Toast } from '../src/Toast'
 import { expect, jest, test, describe } from '@jest/globals'
 
-describe('Satchel: testing bin method', () => {
-  beforeEach(() => {
-    sessionStorage.clear()
+describe('Toast.js test', () => {
+  test('new Toast to creat a toast intance', () => {
+    const toaster = new Toast()
+    expect(toaster.className).toBe('toast')
+  })
+  test('Toast.create() will thow because no message was passed.', () => {
+    const toaster = new Toast()
+    expect(() => toaster.create()).toThrowError(
+      'Toast.js requires a message string!'
+    )
+  })
+  test('Toast.create() returns an id of a new toast.', () => {
+    const toaster = new Toast()
+    expect(toaster.create('first toast!')).toBe('body-0')
+    expect(toaster.create('next toast!')).toBe('body-1')
   })
 
-  test('Satchel: Test bin() to remove an entry from Storage', () => {
-    // localStorage
-    const tacoLocal = new Satchel('taco', { data: null, expiry: null }, true)
-    expect(window.localStorage.length).toEqual(1)
-    tacoLocal.bin()
-    expect(window.localStorage.length).toEqual(0)
-    expect(tacoLocal.get(true)).toBe(null)
-
-    // sessionStorage
-    const tacoSession = new Satchel('taco', { data: null, expiry: null }, false)
-    expect(window.sessionStorage.length).toEqual(1)
-    tacoSession.bin()
-    expect(window.sessionStorage.length).toEqual(0)
-    expect(tacoSession.get(true)).toBe(null)
+  test('Toast.create() adds a toast to the body element.', () => {
+    const toastNode = document.createElement('template')
+    toastNode.innerHTML =
+      '<div class="toast" data-toast-id="body-0" role="alert">first toast!</div>'
+    const toaster = new Toast()
+    expect(toaster.create('first toast!')).toBe('body-0')
+    const body = document.querySelector('body')
+    expect(body.querySelector('[data-toast-id="body-0"]')).toStrictEqual(
+      toastNode.content.firstChild
+    )
   })
 
-  test('Satchel: Test bin() to try and remove a missing record', () => {
-    const noTaco = new Satchel('taco')
-    noTaco.bin()
-
-    expect(noTaco.bin()).toBe(null)
+  test('Toast.destroy() finds and removes toast, and returns Toast() instance.', () => {
+    const toaster = new Toast()
+    const myToast = toaster.create('a toast!')
+    expect(myToast).toBe('body-0')
+    expect(toaster.destroy(myToast).className).toBe('toast')
+  })
+  test('Toast.destroy() retuns false if no data-toast-id ID found.', () => {
+    const toaster = new Toast()
+    expect(toaster.destroy('no-toast')).toBe(false)
   })
 })
